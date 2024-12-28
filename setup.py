60492eb3abe14770d7a84f8c79ad16977c74f1a9
Uday Krishna <udaykrishna@users.noreply.github.com>
2024-12-28 21:53:54 +0530

    Update versioning and project description in setup.py; remove _version.py

--- setup.py
+++ setup.py
@@ -5,15 +5,7 @@ import re
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-
-RE_VER = re.compile(r"^__version__ = ['\"]([^'\"]*)['\"]", re.M)
-
-version = ''
-with open("social_login/_version.py", "rt") as f:
-    ver_cont = f.read()
-    match_version = RE_VER.search(ver_cont)
-    if match_version:
-        version = match_version.group(1)
+version="0.0.0"
 
 library_folder = os.path.dirname(os.path.realpath(__file__))
 requirement_file_path = library_folder + '/requirements.txt'
@@ -28,10 +20,10 @@ setuptools.setup(
     version=version,
     author="Uday Krishna",
     author_email="uday@udaykrishna.com",
-    description="Social Login",
+    description="Py Social Login",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/udaykrishna/py-social-auth/",
+    url="https://github.com/udaykrishna/py-social-login/",
     install_requires=install_requires,
     extras_require=extras_require,
     packages=setuptools.find_packages(),
